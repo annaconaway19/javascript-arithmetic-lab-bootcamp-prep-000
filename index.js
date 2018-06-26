6ea3f8ca1ad7440e672069b4d55b19d92c4b5c30
Anna Conaway <annaconaway19@gmail.com>
2018-06-26 22:32:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+var a = 5;
+var b = 10;
+var n = 10.75;
+
+function add() {
+  a += b;
+}
+
+function subtract() {
+  a -= b;
+}
+
+function multiply() {
+  a *= b;
+}
+
+function divide() {
+  a /= b;
+}
+
+function inc() {
+  n ++;
+}
+
+function dec() {
+  n --;
+}
+
+function makeInt() {
+  parseInt(n, 15);
+}
+
+function makeInt() {
+  parseInt(n, 0);
+}
+
+function preserveDecimal() {
+  parseInt('abc', n);
+}
+
+function preserveDecimal() {
+  parseFloat(n);
+}
